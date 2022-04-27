b29900b96301bada63f84d1128cc2d1dd5caca91
yousef-bigdeli <yousefbigdeli96@gmail.com>
2022-04-27 15:49:51 +0430

    change New comment page structure

--- src/Pages/NewCommentPage.js
+++ src/Pages/NewCommentPage.js
@@ -1,12 +1,30 @@
-import NewComment from "../components/Comment/NewComment";
+import { useState } from "react";
 import { postComment } from "../services/commentServices";
 import { toast } from "react-toastify";
 
 const NewCommentPage = () => {
-  const postCommentHandler = async (comment) => {
+  const [comment, setComment] = useState({
+    name: "",
+    email: "",
+    body: "",
+  });
+
+  const changeInputHandler = (e) => {
+    setComment((prevComment) => {
+      return { ...prevComment, [e.target.name]: e.target.value };
+    });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
     try {
       await postComment(comment);
       toast.success("Comment was added.");
+      setComment({
+        name: "",
+        email: "",
+        body: "",
+      });
     } catch (error) {
       console.log(error);
     }
@@ -14,7 +32,42 @@ const NewCommentPage = () => {
 
   return (
     <section className="flex">
-      <NewComment postComment={postCommentHandler} />
+      <div>
+        <form className="new-comment flex" onSubmit={submitHandler}>
+          <div className="flex">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              onChange={changeInputHandler}
+              value={comment.name}
+            />
+          </div>
+          <div className="flex">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={changeInputHandler}
+              value={comment.email}
+            />
+          </div>
+          <div className="flex">
+            <label htmlFor="body">Body</label>
+            <textarea
+              id="body"
+              name="body"
+              onChange={changeInputHandler}
+              value={comment.body}
+            />
+          </div>
+          <button type="submit" className="add-btn">
+            Add
+          </button>
+        </form>
+      </div>
     </section>
   );
 };
